3f32f9496d072d91784ddfe28d18fd4cb486cd76
Hartaithan <hartaithan@gmail.com>
2021-09-27 21:45:55 +0900

    feat: add figures and text to ThreeJS

--- src/components/ThreeJS.tsx
+++ src/components/ThreeJS.tsx
@@ -1,16 +1,30 @@
 import React from "react";
 import "../styles/threejs.scss";
 import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 
 function randomNumber(min: number, max: number) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 const Figure = React.memo(() => {
-	console.log("RENDER");
-	const randNum = randomNumber(0, 70);
+	const font = useLoader(THREE.FontLoader, "/fonts/roboto.json");
+	const randNum = randomNumber(0, 110);
 	const mesh = React.useRef<THREE.Mesh>(null!);
+	const fontOptions = React.useMemo(
+		() => ({
+			font: font,
+			size: 2,
+			height: 0.3,
+			curveSegments: 2,
+			bevelEnabled: true,
+			bevelThickness: 0,
+			bevelSize: 0.001,
+			bevelOffset: 0,
+			bevelSegments: 1,
+		}),
+		[font]
+	);
 	const material = new THREE.MeshStandardMaterial({ color: "white", wireframe: true, transparent: true, opacity: 0.3 });
 
 	let geometry;
@@ -35,7 +49,7 @@ const Figure = React.memo(() => {
 			break;
 		case randNum >= 31 && randNum <= 40:
 			geometry = new THREE.TorusKnotGeometry(1, 0.1, 160, 10, 3, 5); // ТОР-УЗЕЛ
-			scale = 1.1;
+			scale = 1;
 			rotation = 0.01;
 			break;
 		case randNum >= 41 && randNum <= 50:
@@ -45,13 +59,33 @@ const Figure = React.memo(() => {
 			break;
 		case randNum >= 51 && randNum <= 60:
 			geometry = new THREE.TetrahedronGeometry(1, 0); // ТЕТРАЭДР
-			scale = 2;
-			rotation = 0.008;
+			scale = 1.8;
+			rotation = 0.01;
 			break;
 		case randNum >= 61 && randNum <= 70:
 			geometry = new THREE.TorusGeometry(25, 10, 4, 4); // УГЛОВАТЫЙ ТОР
 			scale = 0.05;
-			rotation = 0.008;
+			rotation = 0.01;
+			break;
+		case randNum >= 71 && randNum <= 80:
+			geometry = new THREE.TextGeometry(":D", fontOptions).center();
+			scale = 1.2;
+			rotation = 0.01;
+			break;
+		case randNum >= 81 && randNum <= 90:
+			geometry = new THREE.TextGeometry("(•_•)", fontOptions).center();
+			scale = 0.7;
+			rotation = 0.01;
+			break;
+		case randNum >= 91 && randNum <= 100:
+			geometry = new THREE.TextGeometry("D:", fontOptions).center();
+			scale = 1.2;
+			rotation = 0.01;
+			break;
+		case randNum >= 101 && randNum <= 110:
+			geometry = new THREE.TextGeometry("H", fontOptions).center();
+			scale = 1.5;
+			rotation = 0.01;
 			break;
 	}
 
@@ -62,10 +96,12 @@ const Figure = React.memo(() => {
 
 const ThreeJS: React.FC = () => {
 	return (
-		<Canvas className="threejs" dpr={window.devicePixelRatio}>
-			<ambientLight />
-			<Figure />
-		</Canvas>
+		<React.Suspense fallback={null}>
+			<Canvas className="threejs" dpr={window.devicePixelRatio}>
+				<ambientLight />
+				<Figure />
+			</Canvas>
+		</React.Suspense>
 	);
 };
 
