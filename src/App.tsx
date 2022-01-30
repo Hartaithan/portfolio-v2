131f2b77d0d262246a8781a913b30ddd576331fc
hartaithan <hartaithan@gmail.com>
2022-01-30 18:25:29 +0900

    chore: update react-router-dom to v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 import Navigation from "./components/Navigation";
@@ -11,17 +11,16 @@ import HomePage from "./pages/HomePage";
 import WorksPage from "./pages/WorksPage";
 
 const App: React.FC = () => {
-  const location = useLocation();
   return (
     <>
       <Navigation />
       <AnimatePresence exitBeforeEnter>
-        <Switch location={location} key={location.key}>
-          <Route component={HomePage} path="/" exact />
-          <Route component={AboutPage} path="/about" />
-          <Route component={WorksPage} path="/works" />
-          <Route component={ContactPage} path="/contact" />
-        </Switch>
+        <Routes key="routes">
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/works" element={<WorksPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
       </AnimatePresence>
       <ThreeJS />
     </>
